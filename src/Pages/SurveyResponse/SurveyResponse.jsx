8b0f7d2c1edf27d03be0e4f7e886317648f7ede3
regan-mu <regansomi@gmail.com>
2023-06-26 15:44:20 +0300

    Change survey page layout

--- src/Pages/SurveyResponse/SurveyResponse.jsx
+++ src/Pages/SurveyResponse/SurveyResponse.jsx
@@ -26,22 +26,24 @@ const SurveyResponse = () => {
                     <div className="survey__info sidebar__sections">
                         <h3>This is the survey title </h3>
                         <p>Estimated survey time: <span>1 minute</span></p>
+                        <p className="note">
+                            When done filling out the survey, take a screenshot of the last survey page and upload below.
+                        </p>
                     </div>
+                    {
+                        surveyStarted && <div className="response__container sidebar__sections">
+                            <form className="response__form">
+                            <label htmlFor="screenshot">Upload Screenshot</label>
+                            <input onChange={handleChange} required id="screenshot" name="screenshot" type="file"  accept="image/jpeg, image/png, image/jpg" />
+                            <button type="submit">Upload</button>
+                            </form>
+                        </div>
+                    }
                     {
                         !surveyStarted &&  <div className="survey__timer sidebar__sections">
                             <button onClick={startSurvey} className="start__survey">Start Survey</button>
                         </div>
                     }
-                    <div className="response__container sidebar__sections">
-                        <p className="note">
-                            When done filling out the survey, take a screenshot of the last survey page and upload below.
-                        </p>
-                        {surveyStarted && <form className="response__form">
-                            <label htmlFor="screenshot">Upload Screenshot</label>
-                            <input onChange={handleChange} required id="screenshot" name="screenshot" type="file"  accept="image/jpeg, image/png, image/jpg" />
-                            <button type="submit">Upload</button>
-                        </form>}
-                    </div>
                 </div>
                 <div className="survey__response-main">
                     {surveyStarted && <iframe src="https://stanforduniversity.qualtrics.com/jfe/form/SV_0D1J7Xiddy3uRj8" frameborder="0"></iframe>}
